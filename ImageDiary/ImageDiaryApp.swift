fa3e5cc0dbc63076fbbd1789c6dec40ff0078d8b
kakutagawa <kaktgw0824@gmail.com>
2024-01-29 18:47:22 +0900

    Initial Commit

---
+++ ImageDiary/ImageDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDiaryApp.swift
+//  ImageDiary
+//
+//  Created by 芥川浩平 on 2024/01/29.
+//
+
+import SwiftUI
+
+@main
+struct ImageDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
